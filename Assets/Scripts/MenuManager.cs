8d708927931a492b755eda9b7475c3ad5d0960cd
Wakana <83waksug@gafe.molndal.se>
2024-04-15 10:12:37 +0200

    Project

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource audioSource;
+    public AudioClip sound;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ToggleAudio()
+    {
+        if (audioSource != null)
+        {
+            // 関数ToggleAudioが呼ばれたら現在のmuteの値を反転させる
+            audioSource.mute = !audioSource.mute;
+        }
+    }
+}
