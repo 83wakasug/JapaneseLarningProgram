bafd127ce8137bae4ece98eac9318e100b83dde2
Wakana <83waksug@gafe.molndal.se>
2024-04-23 11:16:36 +0200

    delete unessesary scripts

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private AudioSource audioSource;
-    public AudioClip sound;
+    public CanvasScaler scaler;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        settingupSize();
     }
 
     public void ToggleAudio()
@@ -20,5 +23,18 @@ public class MenuManager : MonoBehaviour
             // 関数ToggleAudioが呼ばれたら現在のmuteの値を反転させる
             audioSource.mute = !audioSource.mute;
         }
+    }
+
+
+    public void settingupSize()
+    {
+
+        int width = Screen.currentResolution.width;
+        int height = Screen.currentResolution.height;
+        scaler.scaleFactor = height / 1080;
+        RectTransform rect = this.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(width, height);
+ 
+
     }
 }
